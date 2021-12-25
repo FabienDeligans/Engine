1259c363d804562631c25caed11256aac2b420ca
fabienDeligans <deligans@gmail.com>
2021-12-25 22:16:35 +0100

    Refactoring + mise en page

--- BlazorMongoTemplateApp/Pages/TableComponentExemple.razor.cs
+++ BlazorMongoTemplateApp/Pages/TableComponentExemple.razor.cs
@@ -1,47 +1,26 @@
-﻿using BlazorMongoTemplateApp.Component;
-using BlazorMongoTemplateApp.Database;
-using BlazorMongoTemplateApp.Models;
-using Engine.Hubs;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+using BlazorMongoTemplateApp.Component;
 
 namespace BlazorMongoTemplateApp.Pages
 {
-    // TODO inherit from SignalRDataList<T> in .razor file
-    // When Initialize override "Context" and "DataList"
-    // run await InitSignalR()
     public partial class TableComponentExemple
     {
-        private TableComponent<MyEntity> ChildComponent { get; set; }
-        private int Number { get; set; }
+        private TableComponent<Truc> ChildComponentTruc { get; set; }
+        public List<Truc> CustomList { get; set; }
 
-        protected override async Task OnInitializedAsync()
+        protected override void OnInitialized()
         {
-            Context = ContextFactory.MakeContext();
-            await InitSignalR();
-        }
-
-        private async Task Generate()
-        {
-            using var context = ContextFactory.MakeContext();
-
-            var list = new List<MyEntity>();
-            for (var i = 0; i < Number; i++)
+            CustomList = new List<Truc>();
+            for (var i = 0; i < 500; i++)
             {
-                var myEntity = new MyEntity
+                CustomList.Add(new Truc()
                 {
-                    Data = RandomString(10),
-                    Now = DateTime.Now,
-                    Numeric = new Random().Next(0, 11)
-                };
-                list.Add(myEntity);
+                    Nombre = new Random().Next(0, 11),
+                    Name = RandomString(10),
+                });
             }
-            context.InsertAll(list);
-            await InvokeAsync(StateHasChanged);
-            await OnInitializedAsync();
         }
 
         private static readonly Random Random = new();
@@ -52,44 +31,5 @@ namespace BlazorMongoTemplateApp.Pages
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[Random.Next(s.Length)]).ToArray());
         }
-
-        private async Task Drop()
-        {
-            using var context = ContextFactory.MakeContext();
-            context.DropDatabase();
-
-            await RefreshSignalR(null, Crud.Read);
-        }
-
-        private async Task Add()
-        {
-            var entity = new MyEntity
-            {
-                Numeric = new Random().Next(0, 11),
-                Data = RandomString(10),
-                Now = DateTime.Now
-            };
-            using var context = ContextFactory.MakeContext();
-            context.Insert(entity);
-
-            await RefreshSignalR(entity, Crud.Create);
-        }
-
-        private async Task Update(MyEntity entity)
-        {
-            using var context = ContextFactory.MakeContext();
-            context.UpdateEntity(entity);
-
-            await RefreshSignalR(entity, Crud.Update);
-        }
-
-        private async Task Delete(MyEntity entity)
-        {
-            using var context = ContextFactory.MakeContext();
-            context.RemoveOne<MyEntity>(v => v.Id == entity.Id);
-
-            await RefreshSignalR(entity, Crud.Delete);
-        }
-
     }
 }
---
+++ BlazorMongoTemplateApp/Pages/TableComponentSignalRExemple.razor.cs
@@ -0,0 +1,93 @@
+﻿using BlazorMongoTemplateApp.Component;
+using BlazorMongoTemplateApp.Database;
+using BlazorMongoTemplateApp.Models;
+using Engine.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMongoTemplateApp.Pages
+{
+    // TODO inherit from SignalRDataList<T> in .razor file
+    // When Initialize override "Context" and "DataList"
+    // run await InitSignalR()
+    public partial class TableComponentSignalRExemple
+    {
+        private TableComponent<MyEntity> ChildComponent { get; set; }
+        private int Number { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Context = ContextFactory.MakeContext();
+            await InitSignalR();
+        }
+
+        private async Task Generate()
+        {
+            using var context = ContextFactory.MakeContext();
+
+            var list = new List<MyEntity>();
+            for (var i = 0; i < Number; i++)
+            {
+                list.Add(new MyEntity
+                {
+                    Data = RandomString(10),
+                    Now = DateTime.Now,
+                    Numeric = new Random().Next(0, 11)
+                });
+            }
+            context.InsertAll(list);
+            await InvokeAsync(StateHasChanged);
+            await OnInitializedAsync();
+        }
+
+        private static readonly Random Random = new();
+
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+
+        private async Task Drop()
+        {
+            using var context = ContextFactory.MakeContext();
+            context.DropDatabase();
+
+            await RefreshSignalR(null, Crud.Read);
+        }
+
+        private async Task Add()
+        {
+            var entity = new MyEntity
+            {
+                Numeric = new Random().Next(0, 11),
+                Data = RandomString(10),
+                Now = DateTime.Now
+            };
+            using var context = ContextFactory.MakeContext();
+            context.Insert(entity);
+
+            await RefreshSignalR(entity, Crud.Create);
+        }
+
+        private async Task Update(MyEntity entity)
+        {
+            using var context = ContextFactory.MakeContext();
+            context.UpdateEntity(entity);
+
+            await RefreshSignalR(entity, Crud.Update);
+        }
+
+        private async Task Delete(MyEntity entity)
+        {
+            using var context = ContextFactory.MakeContext();
+            context.RemoveOne<MyEntity>(v => v.Id == entity.Id);
+
+            await RefreshSignalR(entity, Crud.Delete);
+        }
+
+    }
+}
