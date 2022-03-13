50cb8d52382be2052d6ef3f65ee586bfc5d242ff
FabienDeligans <deligans@gmail.com>
2022-03-13 12:04:33 +0100

    execute exe

--- BlazorMongoTemplateApp/Pages/Index.razor.cs
+++ BlazorMongoTemplateApp/Pages/Index.razor.cs
@@ -1,6 +1,14 @@
-﻿namespace BlazorMongoTemplateApp.Pages
+﻿using System.Diagnostics;
+
+namespace BlazorMongoTemplateApp.Pages
 {
     public partial class Index 
     {
+        private void Do()
+        {
+            var process = new Process();
+            process.StartInfo.FileName = $@"C:\Program Files\Notepad++\notepad++.exe";
+            process.Start();
+        }
     }
 }
